4238faa641b6690a4df4458161e80a9d0eb86348
Alexey Ershov <alex4rom@gmail.com>
2015-12-15 18:46:29 +0300

    Finish day 2 part 1

--- day_2/day2.rb
+++ day_2/day2.rb
@@ -1,17 +1,24 @@
-def square_dimensions(first, second)
-  2 * first * second
+require 'pry-byebug'
+
+def amount_of_paper(l, w, h)
+  lw, wh, hl = l * w, w * h, h * l
+  2 * (lw + wh + hl) + [lw, wh, hl].min
+end
+
+def min_ribbon_dimensions(l, w, h)
+  arr = [l, w, h]
+  arr.delete(arr.max)
+  arr.inject { |i, j| i * 2 + j * 2 }
 end
 
 @sum_of_all = 0
 
 File.foreach('input.txt') do |line|
 
-  dimensions = line.gsub(/\D/, '').chars
-  min = dimensions.min
+  dimensions = line.split('x')
   l, w, h = dimensions.collect(&:to_i)
 
-  sum = square_dimensions(l, w) + square_dimensions(w, h) + square_dimensions(h, l)
-  @sum_of_all += sum
+  @sum_of_all += amount_of_paper(l, w, h)
 end
 
 puts @sum_of_all
